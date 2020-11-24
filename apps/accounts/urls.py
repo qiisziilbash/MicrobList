2f00dca0e6236f2649608a60846b1ec0666d2a71
Mahdi Shafiei <m.shafiei0423@gmail.com>
2020-11-24 12:35:04 -0600

    url names are updated

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,9 +4,9 @@ from apps.accounts import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('accounts/sign_in/', views.sign_in, name='login'),
-    path('accounts/sign_out/', views.sign_out, name='logout'),
-    path('accounts/sign_up/', views.sign_up, name='forgot'),
+    path('accounts/sign_in/', views.sign_in, name='sign_in'),
+    path('accounts/sign_out/', views.sign_out, name='sign_out'),
+    path('accounts/sign_up/', views.sign_up, name='sign_up'),
     path('accounts/reset/', views.reset, name='reset'),
-    path('accounts/forgot/', views.forgot, name='reset'),
+    path('accounts/forgot/', views.forgot, name='forgot'),
 ]
