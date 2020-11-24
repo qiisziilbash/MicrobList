f11b5196be158e42934a0c38c0d06f9269157817
Mahdi Shafiei <m.shafiei0423@gmail.com>
2020-11-24 12:27:39 -0600

    accounts views are created

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def sign_up(request):
+    pass
+
+
+def sign_in(request):
+    pass
+
+
+def sign_out(request):
+    pass
+
+
+def reset(request):
+    pass
+
+
+def forgot(request):
+    pass
